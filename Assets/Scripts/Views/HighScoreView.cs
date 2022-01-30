6dccb081f2ec40d4c49deecb8e86ed03ed836888
Martín Alejandro Campero <martin.campero19697@gmail.com>
2022-01-30 18:53:39 -0300

    -Implement Toggle Buttons in HighScore View -Import Toggle Button

--- Assets/Scripts/Views/HighScoreView.cs
+++ Assets/Scripts/Views/HighScoreView.cs
@@ -1,18 +1,50 @@
+using Scripts.Controllers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HighScoreView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ToggleController _localGlobalToggle;
+    public ToggleController _allLevelToggle;
+    public Dropdown _levelDropdown;
+
+    private bool isLocal;
+    private bool isAll;
+    private int level;
+
+    private void Start()
     {
-        
+        isLocal = _localGlobalToggle.isOn;
+        isAll = _allLevelToggle.isOn;
+        _levelDropdown.interactable = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isLocal != _localGlobalToggle.isOn)
+        {
+            isLocal = _localGlobalToggle.isOn;
+            Debug.LogWarning("Cambió local a: " + isLocal);
+        }
+        if (isAll != _allLevelToggle.isOn)
+        {
+            isAll = _allLevelToggle.isOn;
+            if (isAll)
+            {
+                _levelDropdown.interactable = false;
+                _levelDropdown.value = 0;
+            }
+            else _levelDropdown.interactable = true;
+            level= _levelDropdown.value+1;
+            Debug.LogWarning("Cambió all a: " + isAll + " - Level: " + level);
+        }
+    }
+
+    public void SetLevel(int levelIndex)
+    {
+        level = levelIndex+1;
+        Debug.Log("Level: " + (levelIndex + 1));
     }
 }
