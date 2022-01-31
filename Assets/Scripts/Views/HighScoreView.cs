d62f13e2a528845636ce61f3694cd3db974d3632
Martín Alejandro Campero <martin.campero19697@gmail.com>
2022-01-30 22:22:02 -0300

    -Create RowPrefab -Create table for highscores

--- Assets/Scripts/Views/HighScoreView.cs
+++ Assets/Scripts/Views/HighScoreView.cs
@@ -1,6 +1,8 @@
 using Scripts.Controllers;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +11,10 @@ public class HighScoreView : MonoBehaviour
     public ToggleController _localGlobalToggle;
     public ToggleController _allLevelToggle;
     public Dropdown _levelDropdown;
+    public List<TextMeshProUGUI> _positionList;
+    public List<TextMeshProUGUI> _playerList;
+    public List<TextMeshProUGUI> _levelList;
+    public List<TextMeshProUGUI> _scoreList;
 
     private bool isLocal;
     private bool isAll;
@@ -16,18 +22,40 @@ public class HighScoreView : MonoBehaviour
 
     private void Start()
     {
+        VariablesInit();
+        LoadTable();
+    }
+
+    private void Update()
+    {
+        LocalGlobalToggleSwitch();
+        AllLevelToggleSwitch();
+    }
+
+    public void SetLevel(int levelIndex)
+    {
+        level = levelIndex+1;
+        Debug.Log("Level: " + (levelIndex + 1));
+    }
+
+    private void VariablesInit()
+    {
         isLocal = _localGlobalToggle.isOn;
         isAll = _allLevelToggle.isOn;
         _levelDropdown.interactable = false;
     }
 
-    private void Update()
+    private void LocalGlobalToggleSwitch()
     {
         if (isLocal != _localGlobalToggle.isOn)
         {
             isLocal = _localGlobalToggle.isOn;
             Debug.LogWarning("Cambió local a: " + isLocal);
         }
+    }
+
+    private void AllLevelToggleSwitch()
+    {
         if (isAll != _allLevelToggle.isOn)
         {
             isAll = _allLevelToggle.isOn;
@@ -37,14 +65,16 @@ public class HighScoreView : MonoBehaviour
                 _levelDropdown.value = 0;
             }
             else _levelDropdown.interactable = true;
-            level= _levelDropdown.value+1;
+            level = _levelDropdown.value + 1;
             Debug.LogWarning("Cambió all a: " + isAll + " - Level: " + level);
         }
     }
 
-    public void SetLevel(int levelIndex)
+    private void LoadTable()
     {
-        level = levelIndex+1;
-        Debug.Log("Level: " + (levelIndex + 1));
+        for(int i = 0; i < 10; i++)
+        {
+            _positionList[i].text = (i + 1).ToString();
+        }
     }
 }
