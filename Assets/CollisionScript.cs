27085aefd3823ab2a833966ef12c95fb798f6809
Martín Alejandro Campero <martin.campero19697@gmail.com>
2022-02-17 18:01:29 -0300

    -Refactor position of GO in scene to make the carrousel -Add new images for backgrounds and streets

--- Assets/CollisionScript.cs
+++ Assets/CollisionScript.cs
@@ -5,7 +5,7 @@ public class CollisionScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         this.transform.localPosition = new Vector3(
-            2000,
+            2050,
             this.transform.localPosition.y,
             this.transform.localPosition.z);
     }
