876fc9b82b2a671d38e294d6f1bb1782c429da5f
Martín Alejandro Campero <martin.campero19697@gmail.com>
2022-03-16 21:14:54 -0300

    -Add animation and random positionY to sick egg -Add explosion and death egg prefab

--- Assets/Scripts/Controllers/InGame/CollectableController.cs
+++ Assets/Scripts/Controllers/InGame/CollectableController.cs
@@ -10,43 +10,49 @@ namespace Scripts.Controllers.InGame
     {
         [SerializeField] private Sprite _sickEggSprite;
         [SerializeField] private Sprite _healthyEggSprite;
+        [SerializeField] private ParticleSystem _explosion;
 
         private Rigidbody2D _rigidbody;
         private SpriteRenderer _currentSprite;
-        private BoxCollider2D _boxCollider2D;
+        private CapsuleCollider2D _capsuleCollider2D;
         
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
             _currentSprite = GetComponent<SpriteRenderer>();
-            _boxCollider2D = GetComponent<BoxCollider2D>();
+            _capsuleCollider2D = GetComponent<CapsuleCollider2D>();
             ResetCollectable();
+            _explosion.Pause();
         }
 
         public void MoveCollectable(int velocity)
         {
             _rigidbody.velocity = new Vector2(-velocity, _rigidbody.velocity.y);
             if((int)this.transform.localPosition.x <= -2000)
-            {
                 ResetCollectable();
-            }
         }
 
         private void ResetCollectable()
         {
             this.transform.localPosition = new Vector3(
                 2000,
-                this.transform.localPosition.y,
-                this.transform.localPosition.z);
+                SetPositionY(),
+                this.transform.localPosition.z);;
             _currentSprite.sprite = _sickEggSprite;
-            _boxCollider2D.enabled = true;
+            _capsuleCollider2D.enabled = true;
         }
 
-        private void OnCollisionEnter2D(Collision2D collision)
+        private void OnTriggerEnter2D(Collider2D collision)
         {
-            _boxCollider2D.enabled = false;
+            _capsuleCollider2D.enabled = false;
+            _explosion.Play();
             _currentSprite.sprite = _healthyEggSprite;
             UIManager.Instance.UpdateEggCount();
+        }
+
+        private int SetPositionY()
+        {
+            return (int)(UnityEngine.Random.Range(-3.25f, 0f) * 120);
         }
     } 
 }
